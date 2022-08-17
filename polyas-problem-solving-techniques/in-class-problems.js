82c946071166f938a933c6dbeac8098321b6eff0
Gregory Testo <gt@gregorytesto.com>
2022-08-17 18:21:03 +0200

    added section for sum of two numbers

--- polyas-problem-solving-techniques/in-class-problems.js
+++ polyas-problem-solving-techniques/in-class-problems.js
@@ -1,3 +1,41 @@
+// Sum of two numbers
+
+// Write a function that takes in two numbers and returns the sum.
+
+
+// Step One: Understand the problem
+
+// Can you paraphrase the problem?
+
+// How many input values will the algorithm expect to receive?
+// What data type are the inputs?
+// What data type and what shape is the output?
+// Is there anything you don't understand that you may have to Google?
+
+// What is the good case for solving the problem?
+// What kind of edge cases can you think of?
+// Are there any other cases that you can think of that you may want to handle?
+// Can you write out an example or two of input(s) and output for each case you came up with?
+
+// Step Two: Devise a plan
+// Are there any helper methods that come to mind when understanding the problem?
+// Are you able to pseudocode an algorithm that meets all of the cases you came up with?
+
+// Step Three: Carry out the plan
+// Where you able to write the code for the pseudocode that you created?
+// Do you need to practice/Google how to use a certain helper method?
+// Did the plan you devised work?
+// Will you have to create a new plan?
+
+// Step Four: Look back
+// Is the algorithm scalable with reference to time-complexity/space-complexity 
+// How can you refactor the code to make it more time/space efficient?
+// Can you refactor the algorithm to be more readable or maintainable?
+
+
+/////////////////////////////////////
+
+
 // Abbreviate a Two Word Name
 
 // Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
